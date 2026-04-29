ac7d198acee906d03d7057f70bb3a1a94595bcb9
patriciapopa01 <patriciapopa01@gmail.com>
2026-04-29 09:07:21 +0300

    seminar9

---
+++ seminar9/src/ro/ase/cts/facade/clase/ReceptieSpirat.java
@@ -0,0 +1,27 @@
+package ro.ase.cts.facade.clase;
+
+public class ReceptieSpirat {
+
+    private Medic medic;
+    private Salon salon;
+
+    public ReceptieSpirat(Medic medic, Salon salon) {
+        this.medic = medic;
+        this.salon = salon;
+    }
+
+    public void intreneazaPacient(Pacient pacient){
+        if(medic.areTrimitere(pacient)){
+            int patLiber = salon.getPatLiber();
+            if(patLiber != -1){
+                System.out.println("Pacientul " + pacient.getNume() + " va fi internat in patul " + patLiber);
+                salon.ocupaPat(patLiber);
+            } else {
+                System.out.println("Nu avem paturi libere! Du te la alt spital");
+            }
+        } else {
+            System.out.println("Nu are trimitere");
+        }
+    }
+
+}
